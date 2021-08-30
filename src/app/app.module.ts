69c28663dec62882e9649479678c600568ef7b56
palmeiraGabriel <86175527+palmeiraGabriel@users.noreply.github.com>
2021-08-30 14:43:47 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { ListaDeContatosComponent } from './components/lista-de-contatos/lista-de-contatos.component';
+import { MenuComponent } from './components/menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContatoComponent,
+    ListaDeContatosComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
